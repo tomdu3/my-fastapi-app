b5ec697f311c1591ccf5df0c3f0e2d81a968a990
Tomislav Dukez <tomdu3@ymail.com>
2025-12-21 15:31:23 +0000

    add get items with id

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello World", "version": "1.0"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {
+            "item_id": item_id,
+            "message": f"Fetching item {item_id}"
+            }
+
